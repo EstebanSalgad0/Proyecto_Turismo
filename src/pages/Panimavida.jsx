529766471ecb6c920747720e8f79db641af941c7
javiermar20000 <javiermar1200@gmail.com>
2024-10-05 22:25:44 -0300

    footer modularizado

--- src/pages/Panimavida.jsx
+++ src/pages/Panimavida.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Link } from 'react-router-dom';
 import '../styles/Panimavida.css'; // Estilos específicos para el componente
+import Footer from './Footer';
 
 const Panimavida = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -196,44 +197,7 @@ const Panimavida = () => {
         </div>
       </section>
 
-      <footer className="footer-section">
-        <div className="footer-container">
-          <div className="footer-logo">
-            <div className="logo logo-1"></div>
-            <div className="logo logo-2"></div>
-            <div className="logo logo-3"></div>
-          </div>
-
-          <div className="footer-column">
-            <h3>Visita Colbún</h3>
-            <ul>
-              <li><a href="#">Sobre Nosotros</a></li>
-              <li><a href="#">Asociados</a></li>
-              <li><a href="#">Políticas de Privacidad</a></li>
-            </ul>
-          </div>
-
-          <div className="footer-column">
-            <h3>Te puede interesar</h3>
-            <ul>
-              <li><a href="#">Municipalidad de Colbún</a></li>
-              <li><a href="#">Chile Travel</a></li>
-              <li><a href="#">Chile Cultura</a></li>
-              <li><a href="#">Parques nacionales Conaf</a></li>
-            </ul>
-          </div>
-
-          <div className="footer-column">
-            <h3>Encuéntranos también en</h3>
-            <ul>
-              <li><a href="#">Facebook</a></li>
-              <li><a href="#">Instagram</a></li>
-              <li><a href="#">YouTube</a></li>
-            </ul>
-          </div>
-        </div>
-      </footer>
-      
+      <Footer />     
 
 
     </div>
