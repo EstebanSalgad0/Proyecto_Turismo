f1613a879f4f61ce8de0b4b6993401ddc7d90b8b
javiermar20000 <javiermar1200@gmail.com>
2024-10-21 19:31:26 -0300

    mejora en los botones sol/luna

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';  // Importar useNavigate para redirigir
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../styles/Header.css';
+import '../styles/Header.css?v=1.1';
 import './i18n'; // Importa el archivo de configuración
 import { useTranslation } from 'react-i18next';
 
