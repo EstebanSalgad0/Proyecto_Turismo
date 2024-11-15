56e3e88f6ab61cf40ca3f06cbf07672b91e577d3
mrpovea <dariel.povea@gmail.com>
2024-11-15 04:40:06 -0300

    Añadidos mapas de la seccion que hacer -> parques y que hacer -> rutas

--- src/pages/Parque.jsx
+++ src/pages/Parque.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../styles/Parque.css?v=1.4'; // Estilos específicos para el componente
@@ -6,8 +6,19 @@ import Header from '../components/Header';
 import '../components/i18n'; // Importa el archivo de configuración
 import { useTranslation } from 'react-i18next';
 import useCarousel from '../components/useCarousel'; // Hook personalizado para el carrusel
+import LeafletMap from '../components/LeafletMap'; // Componente de mapa
 
 const Parque = () => {
+  // Mapa Parque Guaiquivilo
+  const [latParque, setLatParque] = useState(null);
+  const [lngParque, setLngParque] = useState(null);
+  const [isFirstMapParque, setIsFirstMapParque] = useState(true);
+  
+  // Mapa Cavernas los bellotos
+  const [latCavernas, setLatCavernas] = useState(null);
+  const [lngCavernas, setLngCavernas] = useState(null);
+  const [isFirstMapCavernas, setIsFirstMapCavernas] = useState(true);
+
   const { t, i18n } = useTranslation();
   const { currentSlide, nextSlide, prevSlide } = useCarousel(0); // Configurado para 7 slides
   const slideNames = [
@@ -25,7 +36,35 @@ const Parque = () => {
     if (savedLanguage && savedLanguage !== i18n.language) {
       i18n.changeLanguage(savedLanguage); // Cambiar el idioma si es necesario
     }
+    
+    // Fetch data from the Django API (Parque Guaiquivilo)
+    fetch('http://localhost:8000/api/lugares/buscar/?nombre=parque_guaiquivilo') // Cambia el nombre por el lugar turístico que necesites
+    .then(response => response.json())
+    .then(data => {
+      setLatParque(data.latitud);
+      setLngParque(data.longitud);
+    })
+    .catch(error => console.error('Error fetching location data:', error));
+    
+    // Fetch data from the Django API (Cavernas los bellotos)
+    fetch('http://localhost:8000/api/lugares/buscar/?nombre=cavernas_bellotos') // Cambia el nombre por el lugar turístico que necesites
+    .then(response => response.json())
+    .then(data => {
+      setLatCavernas(data.latitud);
+      setLngCavernas(data.longitud);
+    })
+    .catch(error => console.error('Error fetching location data:', error));
   }, [i18n]); // Añadir el estado del idioma como dependencia
+
+  // toggle mapa Parque Guaiquivilo
+  const toggleMapParque = () => {
+    setIsFirstMapParque(!isFirstMapParque);
+  };
+
+  // toggle mapa Cavernas los bellotos
+  const toggleMapCavernas = () => {
+    setIsFirstMapCavernas(!isFirstMapCavernas);
+  };
 
   return (
     <div className="index-container">
@@ -41,15 +80,37 @@ const Parque = () => {
         </div>
       </div>
 
-      {/* Info Section */}
-      <section className="info-section">
-        <div className="info-content">
+      {/* Parque Guaiquivilo */}
+      <div className="info-section1">
+        <section className="map-section">
+          {latParque && lngParque && isFirstMapParque ? (
+            <LeafletMap latitud={latParque} longitud={lngParque} mapId={"parqueMap"} />
+          ) : (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!4v1729508776865!6m8!1m7!1sCAoSLEFGMVFpcE52eG9fOUs1ZkRac2VzYnNNQ3hsYnBpOWFOdnJpcUFUU0VSazhv!2m2!1d-35.87360339666832!2d-71.11635919023739!3f166.054998459084!4f12.54037435121353!5f0.7820865974627469"
+              width="100%"
+              height="1200"
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          )}
+        </section>
+
+        {/* Existing Content Section */}
+        <section className="info-content">
           <h5>{t('UnforgettablePlaces')}</h5>
           <h1>{t('Remember')}</h1>
           <p>{t('ColbunBeauty')}</p>
-          <button className="btn-blue">{t('Discover')}</button>
-        </div>
-      </section>
+          <div className="button-group">
+            <button className="btn-blue" onClick={() => window.open("https://maps.app.goo.gl/GZSD4dNAL8uKZx1N6", "_blank")}>
+              {t('Discover')}
+            </button>
+            <button className="btn-blue2" onClick={toggleMapParque}>
+              <i className="bi bi-geo-alt"></i>
+            </button>
+          </div>
+        </section>
+      </div>
 
       {/* Hero Section 2 */}
       <div className="hero10">
@@ -60,15 +121,37 @@ const Parque = () => {
         </div>
       </div>
 
-      {/* Info Section */}
-      <section className="info-section">
-        <div className="info-content">
+      {/* Cavernas Bellotos */}
+      <div className="info-section1">
+        <section className="map-section">
+          {latCavernas && lngCavernas && isFirstMapCavernas ? (
+            <LeafletMap latitud={latCavernas} longitud={lngCavernas} mapId={"cavernasMap"} />
+          ) : (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!4v1729508776865!6m8!1m7!1sCAoSLEFGMVFpcE52eG9fOUs1ZkRac2VzYnNNQ3hsYnBpOWFOdnJpcUFUU0VSazhv!2m2!1d-35.87360339666832!2d-71.11635919023739!3f166.054998459084!4f12.54037435121353!5f0.7820865974627469"
+              width="100%"
+              height="1200"
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          )}
+        </section>
+
+        {/* Existing Content Section */}
+        <section className="info-content">
           <h5>{t('UnforgettablePlaces')}</h5>
           <h1>{t('Remember')}</h1>
           <p>{t('ColbunBeauty')}</p>
-          <button className="btn-blue">{t('Discover')}</button>
-        </div>
-      </section>
+          <div className="button-group">
+            <button className="btn-blue" onClick={() => window.open("https://maps.app.goo.gl/GZSD4dNAL8uKZx1N6", "_blank")}>
+              {t('Discover')}
+            </button>
+            <button className="btn-blue2" onClick={toggleMapCavernas}>
+              <i className="bi bi-geo-alt"></i>
+            </button>
+          </div>
+        </section>
+      </div>
 
       {/* Carousel Section */}
       <section className="carousel-section1">
--- src/pages/Termas.jsx
+++ src/pages/Termas.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../styles/Termas.css?v=1.6'; // Estilos específicos para el componente
@@ -6,8 +6,19 @@ import Header from '../components/Header';
 import '../components/i18n'; // Importa el archivo de configuración
 import { useTranslation } from 'react-i18next';
 import useCarousel from '../components/useCarousel'; // Hook personalizado para el carrusel
+import LeafletMap from '../components/LeafletMap'; // Componente de mapa
 
 const Termas = () => {
+  // Mapa Termas
+  const [latTermas, setLatTermas] = useState(null);
+  const [lngTermas, setLngTermas] = useState(null);
+  const [isFirstMapTermas, setIsFirstMapTermas] = useState(true);
+  
+  // Mapa Embalse Machicura
+  const [latEmbalse, setLatEmbalse] = useState(null);
+  const [lngEmbalse, setLngEmbalse] = useState(null);
+  const [isFirstMapEmbalse, setIsFirstMapEmbalse] = useState(true);
+
   const { t, i18n } = useTranslation();
   const { currentSlide, nextSlide, prevSlide } = useCarousel(0); // Configurado para 7 slides
   const slideNames = [
@@ -25,7 +36,35 @@ const Termas = () => {
     if (savedLanguage && savedLanguage !== i18n.language) {
       i18n.changeLanguage(savedLanguage); // Cambiar el idioma si es necesario
     }
+    
+    // Fetch data from the Django API (Termas)
+    fetch('http://localhost:8000/api/lugares/buscar/?nombre=termas_rutas') // Cambia el nombre por el lugar turístico que necesites
+    .then(response => response.json())
+    .then(data => {
+      setLatTermas(data.latitud);
+      setLngTermas(data.longitud);
+    })
+    .catch(error => console.error('Error fetching location data:', error));
+    
+    // Fetch data from the Django API (Emblase Machicura)
+    fetch('http://localhost:8000/api/lugares/buscar/?nombre=emblase_machicura') // Cambia el nombre por el lugar turístico que necesites
+    .then(response => response.json())
+    .then(data => {
+      setLatEmbalse(data.latitud);
+      setLngEmbalse(data.longitud);
+    })
+    .catch(error => console.error('Error fetching location data:', error));
   }, [i18n]); // Añadir el estado del idioma como dependencia
+
+  // Toggle mapa Termas
+  const toggleMapTermas = () => {
+    setIsFirstMapTermas(!isFirstMapTermas);
+  };
+  
+  // Toggle mapa Embalse Machicura
+  const toggleMapEmbalse = () => {
+    setIsFirstMapEmbalse(!isFirstMapEmbalse);
+  };
 
   return (
     <div className="index-container">
@@ -41,15 +80,37 @@ const Termas = () => {
         </div>
       </div>
 
-      {/* Info Section */}
-      <section className="info-section">
-        <div className="info-content">
+      {/* Termas */}
+      <div className="info-section1">
+        <section className="map-section">
+          {latTermas && lngTermas && isFirstMapTermas ? (
+            <LeafletMap latitud={latTermas} longitud={lngTermas} mapId={"termasMap"} />
+          ) : (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!4v1729508776865!6m8!1m7!1sCAoSLEFGMVFpcE52eG9fOUs1ZkRac2VzYnNNQ3hsYnBpOWFOdnJpcUFUU0VSazhv!2m2!1d-35.87360339666832!2d-71.11635919023739!3f166.054998459084!4f12.54037435121353!5f0.7820865974627469"
+              width="100%"
+              height="1200"
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          )}
+        </section>
+
+        {/* Existing Content Section */}
+        <section className="info-content">
           <h5>{t('UnforgettablePlaces')}</h5>
           <h1>{t('Remember')}</h1>
           <p>{t('ColbunBeauty')}</p>
-          <button className="btn-blue">{t('Discover')}</button>
-        </div>
-      </section>
+          <div className="button-group">
+            <button className="btn-blue" onClick={() => window.open("https://maps.app.goo.gl/GZSD4dNAL8uKZx1N6", "_blank")}>
+              {t('Discover')}
+            </button>
+            <button className="btn-blue2" onClick={toggleMapTermas}>
+              <i className="bi bi-geo-alt"></i>
+            </button>
+          </div>
+        </section>
+      </div>
 
       {/* Hero Section 2 */}
       <div className="hero17">
@@ -60,15 +121,37 @@ const Termas = () => {
         </div>
       </div>
 
-      {/* Info Section */}
-      <section className="info-section">
-        <div className="info-content">
+      {/* Embalse Machicura */}
+      <div className="info-section1">
+        <section className="map-section">
+          {latEmbalse && lngEmbalse && isFirstMapEmbalse ? (
+            <LeafletMap latitud={latEmbalse} longitud={lngEmbalse} mapId={"embalseMap"} />
+          ) : (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!4v1729508776865!6m8!1m7!1sCAoSLEFGMVFpcE52eG9fOUs1ZkRac2VzYnNNQ3hsYnBpOWFOdnJpcUFUU0VSazhv!2m2!1d-35.87360339666832!2d-71.11635919023739!3f166.054998459084!4f12.54037435121353!5f0.7820865974627469"
+              width="100%"
+              height="1200"
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          )}
+        </section>
+
+        {/* Existing Content Section */}
+        <section className="info-content">
           <h5>{t('UnforgettablePlaces')}</h5>
           <h1>{t('Remember')}</h1>
           <p>{t('ColbunBeauty')}</p>
-          <button className="btn-blue">{t('Discover')}</button>
-        </div>
-      </section>
+          <div className="button-group">
+            <button className="btn-blue" onClick={() => window.open("https://maps.app.goo.gl/GZSD4dNAL8uKZx1N6", "_blank")}>
+              {t('Discover')}
+            </button>
+            <button className="btn-blue2" onClick={toggleMapEmbalse}>
+              <i className="bi bi-geo-alt"></i>
+            </button>
+          </div>
+        </section>
+      </div>
 
       {/* Carousel Section */}
       <section className="carousel-section1">
