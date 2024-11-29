6eb14de4f650bee2ceac74c0167f9fc9ff52c376
mrpovea <dariel.povea@gmail.com>
2024-11-29 02:55:50 -0300

    añadido root de staticfiles para django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -156,6 +156,7 @@ USE_TZ = True # Habilita el soporte para zonas horarias (timezone) en el proyect
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
