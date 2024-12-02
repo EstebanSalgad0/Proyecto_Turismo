aeee2b5410b07ed00fe94f11909beacfc7f483ba
javiermar20000 <javiermar1200@gmail.com>
2024-12-02 20:40:23 -0300

    el video de fondo se reproduce con sonido ahora de forma automatica

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -44,16 +44,16 @@ const Index = () => {
       {/* Hero Section */}
       <div className="hero2">
       <iframe
-        ref={videoRef}
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/7l3B_AjYnhI?autoplay=1&mute=1&loop=1&playlist=7l3B_AjYnhI&vq=hd1080"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+  ref={videoRef}
+  width="560"
+  height="315"
+  src="https://www.youtube.com/embed/7l3B_AjYnhI?autoplay=1&loop=1&playlist=7l3B_AjYnhI&vq=hd1080"
+  title="YouTube video player"
+  frameBorder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  referrerPolicy="strict-origin-when-cross-origin"
+  allowFullScreen
+></iframe>
         <div className="hero-content2">
           <h1>{t('ColbunTitle')}</h1>
           <h2>{t('SubtitleMessage')}</h2>
